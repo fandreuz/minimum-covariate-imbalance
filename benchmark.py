c6fb06fabeeec48d95efa6059b5ea47dadb256d1
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-05-28 08:33:35 +0200

    multiple increasing k1/k2

--- benchmark.py
+++ benchmark.py
@@ -12,20 +12,20 @@ from utils import generate_problems
 
 for n in range(1000, 10001, 1000):
     for nprime in [100000, 250000, 500000, 750000, 1000000]:
-        t = n / 100
-        k1 = int(t/2)
-        k2 = int(3/2*t)
-        while True:
-            print("n {}, n_prime {}, k1 {}, k2 {}".format(n, nprime, k1, k2))
-            l, L_prime = generate_problems(n, nprime, k1, k2)
+        for func in [lambda x: int(x / 3), lambda x: int(x/2), lambda x: x - 2]:
+            k1 = func(n)
+            k2 = func(n)
+            while True:
+                print("n {}, n_prime {}, k1 {}, k2 {}".format(n, nprime, k1, k2))
+                l, L_prime = generate_problems(n, nprime, k1, k2)
 
-            if min_imbalance_solver(l, L_prime) is None:
-                print("Timeout!")
-                continue
+                if min_imbalance_solver(l, L_prime) is None:
+                    print("Timeout!")
+                    continue
 
-            min_imbalance_solver_alt(l, L_prime)
-            min_imbalance_solver_mcnf(l, L_prime)
-            min_imbalance_solver_networkx(l, L_prime)
-            min_imbalance_solver_google(l, L_prime)
-            print("-----")
-            break
+                min_imbalance_solver_alt(l, L_prime)
+                min_imbalance_solver_mcnf(l, L_prime)
+                min_imbalance_solver_networkx(l, L_prime)
+                min_imbalance_solver_google(l, L_prime)
+                print("-----")
+                break
