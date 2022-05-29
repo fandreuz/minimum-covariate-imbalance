8e4aaad61af650d6bf7977a4c5bd7fc73bcec9f0
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-05-29 09:42:23 +0200

    use with to open file. reduce benchmark size

--- benchmark.py
+++ benchmark.py
@@ -13,57 +13,56 @@ from linear_formulation import (
 from utils import generate_problems
 from time import time
 
-time_file = open("benchmark.txt", "w")
+with open("benchmark.txt", "w") as time_file:
+    for n in range(250, 1001, 250):
+        for nprime in [10000, 25000, 50000, 75000, 100000]:
+            for func in [
+                lambda x: int(x / 3),
+                lambda x: int(x / 2),
+                lambda x: x,
+                lambda x: x * 2,
+                lambda x: x * 3,
+            ]:
+                k1 = func(n)
+                k2 = func(n)
+                k = (k1, k2)
 
-for n in range(500, 5001, 500):
-    for nprime in [10000, 25000, 50000, 75000, 100000]:
-        for func in [
-            lambda x: int(x / 3),
-            lambda x: int(x / 2),
-            lambda x: x,
-            lambda x: x * 2,
-            lambda x: x * 3,
-        ]:
-            k1 = func(n)
-            k2 = func(n)
-            k = (k1, k2)
-
-            while True:
-                print(
-                    "n {}, n_prime {}, k1 {}, k2 {}".format(n, nprime, k1, k2)
-                )
-                l, L_prime = generate_problems(n, nprime, k1, k2)
-
-                start = time()
-                try:
-                    A = compute_A(L_prime, k, nprime)
-                except:
+                while True:
                     print(
-                        "An error occurred while computing A. L_prime: {}, k: {}".format(
-                            L_prime, j
-                        )
+                        "n {}, n_prime {}, k1 {}, k2 {}".format(n, nprime, k1, k2)
                     )
-                    continue
-                print("A computed in {}s".format(time() - start))
+                    l, L_prime = generate_problems(n, nprime, k1, k2)
 
-                start = time()
-                U = compute_U(A, k1)
-                print("U computed in {}s".format(time() - start))
+                    start = time()
+                    try:
+                        A = compute_A(L_prime, k, nprime)
+                    except:
+                        print(
+                            "An error occurred while computing A. L_prime: {}, k: {}".format(
+                                L_prime, j
+                            )
+                        )
+                        continue
+                    print("A computed in {}s".format(time() - start))
 
-                if (
-                    min_imbalance_solver(l, L_prime, A=A, time_file=time_file)
-                    is None
-                ):
-                    print("Timeout!")
-                    continue
+                    start = time()
+                    U = compute_U(A, k1)
+                    print("U computed in {}s".format(time() - start))
 
-                min_imbalance_solver_alt(l, L_prime, A=A, time_file=time_file)
-                min_imbalance_solver_mcnf(l, L_prime, A=A, time_file=time_file)
-                min_imbalance_solver_networkx(
-                    l, L_prime, A=A, U=U, time_file=time_file
-                )
-                min_imbalance_solver_google(
-                    l, L_prime, A=A, U=U, time_file=time_file
-                )
-                print("-----")
-                break
+                    if (
+                        min_imbalance_solver(l, L_prime, A=A, time_file=time_file)
+                        is None
+                    ):
+                        print("Timeout!")
+                        continue
+
+                    min_imbalance_solver_alt(l, L_prime, A=A, time_file=time_file)
+                    min_imbalance_solver_mcnf(l, L_prime, A=A, time_file=time_file)
+                    min_imbalance_solver_networkx(
+                        l, L_prime, A=A, U=U, time_file=time_file
+                    )
+                    min_imbalance_solver_google(
+                        l, L_prime, A=A, U=U, time_file=time_file
+                    )
+                    print("-----")
+                    break
